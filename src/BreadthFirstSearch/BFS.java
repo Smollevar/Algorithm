5316bb48772bcc80d29f344a6ee699ea9bb91662
Dmitriy Kravtsov <e4ahl1bhyr@gmail.com>
2022-02-23 11:25:44 +0700

    Add memoization in BFS

--- src/BreadthFirstSearch/BFS.java
+++ src/BreadthFirstSearch/BFS.java
@@ -1,36 +1,39 @@
 package BreadthFirstSearch;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class BFS {
     public static void main(String[] args) {
-        HashMap<String, String[]> graph = new HashMap<>();
-        graph.put("you", new String[]{"alice", "bob", "claire"});
+        Map<String, String[]> graph = new HashMap<>();
+        graph.put("you", new String[]{"alice", "bob", "bob", "clairea"});
+        graph.put("bob", new String[]{"peggy"});
         graph.put("alice", new String[]{"anuj", "peggy"});
         graph.put("bob", new String[]{"peggy"});
-        graph.put("claire", new String[]{"thom", "jonnya"});
+        graph.put("clairea", new String[]{"thom", "jonny"});
         graph.put("anuj", new String[]{});
         graph.put("peggy", new String[]{});
         graph.put("thom", new String[]{});
-        graph.put("jonnya", new String[]{});
+        graph.put("jonny", new String[]{});
 
         String[] array = graph.get("you");
         Queue<String> queue = new LinkedList<>(Arrays.asList(array));
 
+        Set<String> memoization = new HashSet<>();
+
         while (!queue.isEmpty()) {
             String person = queue.remove();
-            System.out.print(person + " ");
+
             String c = person.substring(person.length() - 1);
-            System.out.println(c);
-            if (c.equals("a")){
+
+            if (memoization.contains(person))
+                continue;
+            if (c.equals("a")) {
                 System.out.println(person + " is a mango seller");
                 queue.clear();
             } else {
-                String [] temp = graph.get(person);
+                String[] temp = graph.get(person);
                 queue.addAll(Arrays.asList(temp));
+                memoization.add(person);
             }
         }
     }
